9168d9a5505aa378b9bc4e6398ea59fbc694e46a
Jaime Irazabal <jaimeirazabal1@gmail.com>
2023-01-12 19:52:34 -0400

    yi

--- script.py
+++ script.py
@@ -15,19 +15,18 @@ file_list = ['example.csv']
 for file in file_list:
     with open(file, 'r') as f:
         reader = csv.reader(f,delimiter=";")
-        # Get column names from the first row of the CSV
-        column_names = ['id','rut','dv','nombre','sexo','direccion','comuna','region','pension_1','pension_2','ley_de_quiebra','neitcom']
-        # REG	    RUT	    dv	APELLIDOSNOMBRES	SEXO	DIRECCION	ID_COMUNA	ID_REGION	PENSION_1	    PENSION_2	                ley_quiebra	neitcom
-        # 128851	2924468	5	ZUnIGA HERMOSILLA ISOLINA DEL CARMEN	FEMENINO	PB 11 DE SEPT E RIQUELME 464	150	8	IPS	NULL	0	        0
-        # 128852	2924479	0	LAFQUEN ACUM CARLOS	MASCULINO	VICENTE REYES 222	247	10	IPS	NULL	0	0
         
+        # campos de validacion externa
+        column_names = ['id','rut','dv','nombre','sexo','direccion','comuna','region','pension_1','pension_2','ley_de_quiebra','neitcom']
+        # obvia la primera columna
         paraquenotomelaprimera = next(reader)
+        # coloca los nombres en comas para el insert
         column_names_str = ', '.join(column_names)
         for row in reader:
    
-            # fila = [row.replace("'",'')]
+            # crea el string con sus values
             valores = ",".join("'" + x.replace("'",'') + "'" for x in row)
-            
+            # commet
             query = "INSERT INTO {} ({}) VALUES ({})".format(table_name, column_names_str, valores)
             print(row)
             print(query)
